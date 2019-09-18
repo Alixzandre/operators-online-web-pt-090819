22068ba9b5e35982a638cbca35b3b44dd0aa861b
Alexander Bispo <alexbispo173@gmail.com>
2019-09-18 01:58:35 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,9 @@
 def unsafe?(speed)
-if speed < 40
-  return true
-else
-  return false
-end
+  if speed < 40 || speed > 60 
+    return true
+  else
+    return false
+  end
 end
 
 
@@ -11,6 +11,8 @@ end
 def not_safe?(speed)
 	speed < 40 ? return true : return false
 end
+
+
 	
 
 
