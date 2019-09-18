14f9fd641fe6b43a341235cb87a86bb68eaaada0
Alexander Bispo <alexbispo173@gmail.com>
2019-09-18 01:51:00 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,11 @@
 def unsafe?(speed)
-
+conditional ? action_if_true : action_if_falseconditional ? action_if_true : action_if_false
 end
 
 
 
 def not_safe?(speed)
-	
+	conditional ? action_if_true : action_if_false
 end
 	
 
